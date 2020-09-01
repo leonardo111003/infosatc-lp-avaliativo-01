5e5fb547b1aa1122420bd814c37b2b24a3fd1398
leonardo111003 <leocatra123@hotmail.com>
2020-09-01 11:11:40 -0300

    fix: aaltera variavel

--- exercicio20.py
+++ exercicio20.py
@@ -1,3 +1,3 @@
-quilogramas  =  float ( input ( "Insira o peso em Kg:" ))
-libras  =  quilogramas  /  1 , 45
+quilograma  =  float ( input ( "Insira o peso em Kg:" ))
+libras  =  quilogramas  /  0 , 45
 print ( libras )
