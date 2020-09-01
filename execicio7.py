40953dcd0e023a26c9d41693a0c016a94156759b
leonardo111003 <69396032+leonardo111003@users.noreply.github.com>
2020-09-01 12:06:56 -0300

    Update execicio7.py

--- execicio7.py
+++ execicio7.py
@@ -1,3 +1,3 @@
 celsius    =    float ( input ( "Insira a temperatura em celsius:" ))
-Fahrenheit    =    celsius    * ( 9    /    5 ) +    32
+Fahrenheit    =    celcius* ( 9    /    5 ) +    32
 print ( Fahrenheit )
