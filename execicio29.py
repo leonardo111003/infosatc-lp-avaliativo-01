3a702628fe3c62e894c1484159a011298bf34a17
leonardo111003 <leocatra123@hotmail.com>
2020-09-01 11:39:42 -0300

    feat: adiciona exercicio 29

---
+++ execicio29.py
@@ -0,0 +1,7 @@
+nota1  =  float ( input ( "Insira a nota um:" ))
+nota2  =  float ( input ( "Insira a nota dois:" ))
+nota3  =  float ( input ( "Insira a nota três:" ))
+nota4  =  float ( input ( "Insira a nota quatro:" ))
+soma  =  nota1  +  nota2  +  nota3  +  nota4
+media  =  float ( soma ) /  4
+print ( media )
